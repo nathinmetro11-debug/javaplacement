b7e1d0265d338b630c06d555f726e655e26d2f8c
Nathin <nathinmetro11@gmail.com>
2026-08-23 09:37:45 +0530

    Add reverse number program

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int reverse = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+
+        System.out.println("Reverse = " + reverse);
+
+        sc.close();
+    }
+}
