60e0ae537aec7e139fd4ddc77abb6bab7d7ca325
Nathin <nathinmetro11@gmail.com>
2026-08-23 09:40:54 +0530

    Add sum of odd digits program

---
+++ SumOfOddDigits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumOfOddDigits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+
+            if (digit % 2 != 0) {
+                sum = sum + digit;
+            }
+
+            number = number / 10;
+        }
+
+        System.out.println("Sum of odd digits = " + sum);
+
+        sc.close();
+    }
+}
