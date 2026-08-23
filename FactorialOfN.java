9f0fe25e02e5f1a7788d92b4c0b5f49280b71e26
Nathin <nathinmetro11@gmail.com>
2026-08-23 09:55:04 +0530

    Add factorial program

---
+++ FactorialOfN.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorialOfN {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial = " + factorial);
+
+        sc.close();
+    }
+}
